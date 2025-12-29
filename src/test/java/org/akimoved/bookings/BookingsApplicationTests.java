00373f8aaa4481eab343a6f6e4b307be3a2cd453
Egor Akimov <e.akimov@mail.astondevs.ru>
2025-12-29 17:00:08 +0400

    initial commit

---
+++ src/test/java/org/akimoved/bookings/BookingsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.akimoved.bookings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
